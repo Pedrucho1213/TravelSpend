40a0d58765708a3011503da4c5edb218d92f2ea1
David <gorrillaz.de.oz@gmail.com>
2021-11-26 18:39:35 -0600

    cambios

---
+++ app/src/main/java/com/example/travelspend/ModalBottomSheet.kt
@@ -0,0 +1,13 @@
+package com.example.travelspend
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+open class ModalBottomSheet : BottomSheetDialogFragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.sheetbottom, container, false)
+    }
+}
