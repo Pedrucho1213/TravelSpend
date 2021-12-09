66db2f073662c3a971c2236e0cffbd79dc7aff25
David <gorrillaz.de.oz@gmail.com>
2021-12-09 12:40:42 -0600

    cambios del jueve 09/Dic

--- app/src/main/java/com/example/travelspend/AddExpense.kt
+++ app/src/main/java/com/example/travelspend/AddExpense.kt
@@ -1,7 +1,9 @@
 package com.example.travelspend
 
+import android.content.ContentValues
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.travelspend.databinding.ActivityAddExpenseBinding
 
 class AddExpense : AppCompatActivity() {
@@ -15,9 +17,37 @@ class AddExpense : AppCompatActivity() {
 
         addGasto.backButton.setOnClickListener { goBack() }
 
+
+        addGasto.addexpense.setOnClickListener {
+        saveExpense()
+        }
     }
 
     private fun goBack(){
         onBackPressed()
     }
+
+    private fun saveExpense() {
+        val admin = DataBase(this, "bd", null, 1)
+        val db = admin.writableDatabase
+        val record = ContentValues()
+        record.put("monto", addGasto.txtMoneyDepense.text.toString().toDouble())
+        record.put("moneda", addGasto.txtMoneyType.text.toString())
+        record.put("concepto",addGasto.txtConceptDepense.text.toString()  )
+        record.put("fecha", addGasto.txtDateDepense.text.toString() )
+        record.put("mpago", addGasto.txtModePayment.text.toString() )
+        record.put("descrip", addGasto.txtDescriptionDepense.text.toString() )
+        db.insert("gastos", null, record)
+        db.close()
+        addGasto.txtMoneyDepense.text = null
+        addGasto.txtMoneyType.text = null
+        addGasto.txtConceptDepense.text = null
+        addGasto.txtDateDepense.text = null
+        addGasto.txtModePayment.text = null
+        addGasto.txtDescriptionDepense.text = null
+
+        record.clear()
+        Toast.makeText(this, "El gasto ha sido agregado", Toast.LENGTH_SHORT).show()
+    }
+
 }
